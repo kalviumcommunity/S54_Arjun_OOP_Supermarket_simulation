3704af2d2788f02f1cdaa9359c9c624e6dcf509a
Arjun <arjun15310@gmail.com>
2024-09-24 23:42:04 +0530

    classes and objects

---
+++ src/Customer.java
@@ -0,0 +1,32 @@
+package src;
+import java.util.ArrayList;
+
+public class Customer {
+    private String name;
+    private int customerID;
+    private ArrayList<Product> shoppingList;
+
+    public Customer(String name, int customerID) {
+        this.name = name;
+        this.customerID = customerID;
+        this.shoppingList = new ArrayList<>();
+    }
+
+    public void displayCustomerDetails() {
+        System.out.println("Customer Name: " + this.name);
+        System.out.println("Customer ID: " + this.customerID);
+    }
+
+    public void addProductToShoppingList(Product product) {
+        shoppingList.add(product);
+        System.out.println(product.name + " added to " + this.name + "'s shopping list.");
+    }
+
+    public void displayShoppingList() {
+        System.out.println(this.name + "'s Shopping List:");
+        for (Product product : shoppingList) {
+            product.displayDetails();
+            System.out.println("----------------------");
+        }
+    }
+}
---
+++ src/Product.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Product {
+    protected String name;
+    protected double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public void displayDetails() {
+        System.out.println("Product Name: " + this.name);
+        System.out.println("Price: $" + this.price);
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+}
